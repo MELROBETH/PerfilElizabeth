c3b2b4e3614c95ff1a7325f6a2b3e24f70a46aea
MELROBETH <163427445+MELROBETH@users.noreply.github.com>
2024-11-14 07:58:04 -0300

    gnvcb

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-/*fim do menu*/
 
